7ea8117457e7d839ac6bafc99799361389e1516d
gyunho <gyunh01@pukyong.ac.kr>
2025-02-18 01:04:59 +0900

    refactor: Mentoring에 mentor 정보 추가

--- Server/B3/src/main/java/com/f1b3/b3/entity/User.java
+++ Server/B3/src/main/java/com/f1b3/b3/entity/User.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,6 +32,12 @@ public class User {
 
     @Column(name = "profile_url")
     private String profileUrl;
+
+    @OneToMany(mappedBy = "mentor")
+    private List<Mentoring> mentorings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Apply> applies = new ArrayList<>();
 
     @Builder
     public User(String name, String emailAddress, String phoneNumber, String description, String profileUrl) {
