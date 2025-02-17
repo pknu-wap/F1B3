d7b7db8c71ff34de14fdd568afda150b41f75e94
김민석|Minseok Kim <alstjr7437@naver.com>
2025-02-17 21:23:06 +0900

    feat: 상수 선언 구현, Base View와 VC를 변경

---
+++ iOS/F1B3/F1B3/Utility/Enums/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  F1B3
+//
+//  Created by KimMinSeok on 2/17/25.
+//
+
+enum Constants {
+    enum Texts {
+        static let appName = "SeMentor"
+        static let mentoringCellIdentifier = "MentoringCell"
+        static let searchIcon = "magnifyingglass"
+        static let color = "AppColor"
+    }
+}
