be5628cadbc9a7fa7c02e5e862bd3a8a2d00418d
김민석|Minseok Kim <alstjr7437@naver.com>
2025-02-17 15:15:20 +0900

    feat: 온보딩 화면 세팅

---
+++ iOS/F1B3/F1B3/Onboarding/View/OnboardingView.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingView.swift
+//  F1B3
+//
+//  Created by KimMinSeok on 2/17/25.
+//
+
+import UIKit
+
+final class OnboardingView: BaseView {
+    
+}
---
+++ iOS/F1B3/F1B3/Onboarding/ViewController/OnboardingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingViewController.swift
+//  F1B3
+//
+//  Created by KimMinSeok on 2/17/25.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+}
